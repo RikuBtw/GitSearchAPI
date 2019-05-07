a29ea26219ec082b0b0f2cc09c4aebe6db4df076
Julien Aubert <j.aubert@pepperbay.fr>
2019-05-07 16:01:06 +0200

    feat: tentative auth

--- server.js
+++ server.js
@@ -1,79 +1,96 @@
-const fetch = require("node-fetch");
+require('dotenv').config()
+
 const express = require('express');
-const env = require('dotenv').config()
+const session = require('express-session');
+const request = require('request');
+const fetch = require("node-fetch");
+const randomString = require('randomstring');
+const qs = require('qs');
 const _ = require('lodash');
 
-const accessToken = env.parsed.GIT_HOST;
 const apiUrl = 'https://api.github.com/graphql';
+const port = process.env.PORT || 3000;
 
-// Create an express server and a GraphQL endpoint
+// Create an express server
 const app = express();
 
-const fetchQuery = (query, res) => {
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
+app.use(
+    session({
+        secret: randomString.generate(),
+        resave: false,
+        saveUninitialized: false
+    })
+);
+
+const fetchQuery = (req, res, query) => {
+    if (!req.session.access_token){
+        res.redirect('/');
+        return;
+    }
     return fetch(apiUrl, {
         method: 'POST',
         body: JSON.stringify({ query }),
         headers: {
-            'Authorization': `Bearer ${accessToken}`,
+            'Authorization': `Bearer ${req.session.access_token}`,
         },
     });
 }
 
-const fetchQueryAndSend = (query, res) => {
-    fetchQuery(query, res)
-    .then(res => res.text())
-    .then(body => res.status(200).send(body))
+const fetchQueryAndSend = (req, res, query) => {
+    const fetchedQuery = fetchQuery(req, res, query);
+    if (!fetchedQuery) return;
+    fetchedQuery
+        .then(res => res.text())
+        .then(body => res.status(200).send(body));
 }
 
-console.log('Available requests :');
-
-//Organization infos
-console.log('/organization/{organization}');
-app.get('/organization/:organization', function (req, res) {
-    const query = `
-        query {
-            organization(login:"${req.params.organization}") {
-                name
-                description
-                avatarUrl
-                location
-                url
-                email
-            }
-        }
-    `;
-    fetchQueryAndSend(query, res);
+app.post('/login', (req, res) => {
+    res.redirect(process.env.AUTH_HOST);
 });
 
-//All users
-console.log('/organization/{organization}/users');
-app.get('/organization/:organization/users', function (req, res) {
-    const query = `
-        query {
-            organization(login:"${req.params.organization}") {
-                membersWithRole(first:18` + (req.query.after ? `, after:"${req.query.after}"` : ``) + `) {
-                    totalCount
-                        nodes {
-                            login
-                            avatarUrl
-                        }
-                    pageInfo {
-                        hasNextPage
-                        endCursor
-                    }
-                }
+app.all('/redirect', (req, res) => {
+    const code = req.query.code;
+    const returnedState = req.query.state;
+    const csrf = req.query.csrf;
+    if (csrf === returnedState) {
+        request.post(
+            {
+                url:
+                    'https://github.com/login/oauth/access_token?' +
+                    qs.stringify({
+                        client_id: process.env.CLIENT_ID,
+                        client_secret: process.env.CLIENT_SECRET,
+                        code: code,
+                        redirect_uri: process.env.HOST + "/redirect",
+                        state: csrf
+                    })
+            },
+            (error, response, body) => {
+                req.session.access_token = qs.parse(body).access_token;
+                res.redirect(process.env.CLIENT_HOST);
             }
-        }
-    `;
-    fetchQueryAndSend(query, res);
+        );
+    } else {
+        res.redirect('/');
+    }
 });
+
+app.all('/', function (req, res) {
+    res.status(200).send([{'token': req.session.access_token}]);
+});
+
 
 //Users infos
-console.log('/user/{login}');
-app.get('/user/:login', function (req, res) {
+app.get('/viewer', function (req, res) {
     const query = `
         query {
-            user(login:"${req.params.login}") {
+            viewer {
                 login
                 name
                 bio
@@ -97,11 +114,10 @@ app.get('/user/:login', function (req, res) {
             }
         }
     `;
-    fetchQueryAndSend(query, res);
+    fetchQueryAndSend(req, res, query);
 });
 
 //Users organizations
-console.log('/user/{login}/organizations');
 app.get('/user/:login/organizations', function (req, res) {
     const query = `
         query {
@@ -114,7 +130,7 @@ app.get('/user/:login/organizations', function (req, res) {
             }
         }
     `;
-    fetchQuery(query, res)
+    fetchQuery(req, res, query)
         .then(res => res.text())
         .then(body => {
             organizations = JSON.parse(body).data.user.organizations.nodes;
@@ -123,12 +139,50 @@ app.get('/user/:login/organizations', function (req, res) {
         })
 });
 
+//Organization infos
+app.get('/organization/:organization', function (req, res) {
+    const query = `
+        query {
+            organization(login:"${req.params.organization}") {
+                name
+                description
+                avatarUrl
+                location
+                url
+                email
+            }
+        }
+    `;
+    fetchQueryAndSend(req, res, query);
+});
+
+//All users
+app.get('/organization/:organization/users', function (req, res) {
+    const query = `
+        query {
+            organization(login:"${req.params.organization}") {
+                membersWithRole(first:18` + (req.query.after ? `, after:"${req.query.after}"` : ``) + `) {
+                    totalCount
+                        nodes {
+                            login
+                            avatarUrl
+                        }
+                    pageInfo {
+                        hasNextPage
+                        endCursor
+                    }
+                }
+            }
+        }
+    `;
+    fetchQueryAndSend(req, res, query);
+});
+
 //User contributions
 function isValidNumberLikes(str) {
     var n = Math.floor(Number(str));
     return n !== Infinity && String(n) === str && n >= 0;
 }
-console.log('/user/{login}/contributions');
 app.get('/user/:login/contributions', function (req, res) {
     const query = `
         query {
@@ -155,7 +209,7 @@ app.get('/user/:login/contributions', function (req, res) {
             }
         }   
     `;
-    fetchQuery(query, res)
+    fetchQuery(req, res, query)
         .then(res => res.text())
         .then(body => {
             pullRequests = JSON.parse(body).data.user.pullRequests.nodes;
@@ -166,12 +220,11 @@ app.get('/user/:login/contributions', function (req, res) {
         })
 });
 
-//3 most stared repo
-console.log('/organization/{organization}/repositories');
+//5 most stared repo
 app.get('/organization/:organization/repositories', function (req, res) {
     const query = `
         query {
-            search(query: "org:${req.params.organization}", type: REPOSITORY, first: 3) {
+            search(query: "org:${req.params.organization}", type: REPOSITORY, first: 5) {
                 repositoryCount
                 edges {
                     node {
@@ -196,7 +249,7 @@ app.get('/organization/:organization/repositories', function (req, res) {
             }
         }
     `;
-    fetchQueryAndSend(query, res);
+    fetchQueryAndSend(req, res, query);
 });
 
-app.listen(4000, () => console.log('\nExpress GraphQL Server Now Running On localhost:4000'));
+app.listen(port, () => console.log('\nAPI Now Running on port:' + port));
